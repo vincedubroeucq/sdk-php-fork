693b0723b08c4d928fda39c5a5f44fe98b875ee4
hermann_sorais <hermannsorais@gmail.com>
2022-05-08 09:05:19 +0200

    Allow null constructor parameters

--- src/OpenAgendaSdk.php
+++ src/OpenAgendaSdk.php
@@ -16,27 +16,39 @@ class OpenAgendaSdk
     private $client;
 
     /**
-     * OpenAgendaSDK constructor.
+     * @var string|null
+     */
+    private $publicKey;
+
+    /**
+     * @var array|null
+     */
+    private $clientOptions;
+
+    /**
+     * OpenAgendaSdk constructor.
      *
-     * @param string $publicKey
-     *  OpenAgenda API publicKey.
+     * @param string|null $publicKey
      * @param array|null $clientOptions
-     *  HttpClient options.
-     * @throws \Exception
      *
      * @see \OpenAgendaSdk\RequestOptions for a list of available client options.
      */
-    public function __construct(string $publicKey, ?array $clientOptions = [])
+    public function __construct(?string $publicKey, ?array $clientOptions = [])
     {
-        $this->client = new HttpClient($publicKey, $clientOptions);
+        $this->publicKey = $publicKey;
+        $this->clientOptions = $clientOptions;
     }
 
     /**
      * @return HttpClient
      */
+    /**
+     * @return HttpClient
+     * @throws \Exception
+     */
     public function getClient(): HttpClient
     {
-        return $this->client;
+        return $this->client ?? new HttpClient($this->publicKey, $this->clientOptions);
     }
 
     /**
@@ -50,7 +62,7 @@ class OpenAgendaSdk
      */
     public function getAgenda(int $agendaUid): string
     {
-        $content = $this->client->request(Endpoints::AGENDA, ['agendaUid' => $agendaUid]);
+        $content = $this->getClient()->request(Endpoints::AGENDA, ['agendaUid' => $agendaUid]);
 
         return $content;
     }
@@ -68,7 +80,7 @@ class OpenAgendaSdk
      */
     public function getEvents(int $agendaUid, ?array $params = []): string
     {
-        $content = $this->client->request(Endpoints::EVENTS, ['agendaUid' => $agendaUid], $params + ['includeLabels' => 1, 'detailed' => 1]);
+        $content = $this->getClient()->request(Endpoints::EVENTS, ['agendaUid' => $agendaUid], $params + ['includeLabels' => 1, 'detailed' => 1]);
 
         return $content;
     }
@@ -86,7 +98,7 @@ class OpenAgendaSdk
      */
     public function getEvent(int $agendaUid, int $eventUid): string
     {
-        $content = $this->client->request(Endpoints::EVENT, ['agendaUid' => $agendaUid, 'eventUid' => $eventUid, 'includeLabels' => 1], ['includeLabels' => 1, 'detailed' => 1]);
+        $content = $this->getClient()->request(Endpoints::EVENT, ['agendaUid' => $agendaUid, 'eventUid' => $eventUid, 'includeLabels' => 1], ['includeLabels' => 1, 'detailed' => 1]);
 
         return $content;
     }
